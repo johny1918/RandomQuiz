077a3d5d239cf5e40ddf8d3cd033fe2edbe9afd4
Ioan Andrasi <andrasi.ioan90@gmail.com>
2025-11-04 22:51:37 +0200

    Finishing setting up server and implement health check

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -2,7 +2,8 @@ use sqlx::PgPool;
 use sqlx::postgres::PgPoolOptions;
 
 pub async fn connect_db() -> Result<PgPool, sqlx::Error> {
-    let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    dotenv::dotenv().ok();
+    let db_url = dotenv::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let pool = PgPoolOptions::new()
         .max_connections(5)
         .connect(&db_url)
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
+use axum::{Json, Router};
+use axum::routing::get;
+use serde_json::json;
+
 mod models;
 mod db;
+use crate::db::connect_db;
+use crate::models::state::AppState;
 
 #[tokio::main]
 async fn main() {
-    //TBD
+    let pool = connect_db().await.unwrap();
+    let app_state = AppState::new(pool);
+    let app = routes(app_state).await;
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000").await.unwrap();
+    println!("Listening on http://127.0.0.1:8000");
+    axum::serve(listener, app.into_make_service()).await.unwrap()
+}
+
+async fn routes(poll: AppState) -> Router {
+    Router::new()
+        .route("/", get(health_check))
+        .with_state(poll)
+}
+
+async fn health_check() -> Json<serde_json::Value> {
+    Json(json!({ "status": "ok" }))
 }
